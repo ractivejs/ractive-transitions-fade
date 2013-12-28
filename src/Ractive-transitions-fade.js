971e0c59c932c57bf2a04ba03942d99431636807
Rich Harris <richard.a.harris@gmail.com>
2013-12-27 22:54:56 -0500

    fix & rebuild

--- src/Ractive-transitions-fade.js
+++ src/Ractive-transitions-fade.js
@@ -25,7 +25,7 @@
 
 	Troubleshooting: If you're using a module system in your app (AMD or
 	something more nodey) then you may need to change the paths below,
-	where it says `require( 'ractive' )` or `define([ 'Ractive' ]...)`.
+	where it says `require( 'Ractive' )` or `define([ 'Ractive' ]...)`.
 
 	==========================
 
@@ -52,7 +52,7 @@
 
 	// Common JS (i.e. browserify) environment
 	if ( typeof module !== 'undefined' && module.exports && typeof require === 'function' ) {
-		factory( require( 'ractive' ) );
+		factory( require( 'Ractive' ) );
 	}
 
 	// AMD?
