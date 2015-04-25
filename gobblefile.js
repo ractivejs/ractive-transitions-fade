c8cfd976bc3667b3e1d2552d0df67ed476c23030
Rich-Harris <richard.a.harris@gmail.com>
2015-04-25 12:03:48 -0400

    -> 0.2.1 - include minified version in dist

--- gobblefile.js
+++ gobblefile.js
@@ -2,14 +2,21 @@ var gobble = require( 'gobble' );
 
 gobble.cwd( __dirname );
 
-module.exports = gobble( 'src' )
-.transform( 'babel', {
-	blacklist: [ 'es6.modules', 'useStrict' ],
-	sourceMap: false
-})
-.transform( 'esperanto-bundle', {
-	entry: 'ractive-transitions-fade',
-	type: 'umd',
-	name: 'Ractive.transitions.fade',
-	sourceMap: false
-});
+var lib = gobble( 'src' )
+	.transform( 'babel', {
+		blacklist: [ 'es6.modules', 'useStrict' ],
+		sourceMap: false
+	})
+	.transform( 'esperanto-bundle', {
+		entry: 'ractive-transitions-fade',
+		type: 'umd',
+		name: 'Ractive.transitions.fade',
+		sourceMap: false
+	});
+
+module.exports = gobble([
+	lib,
+	lib.transform( 'uglifyjs', {
+		ext: '.min.js'
+	})
+]);
